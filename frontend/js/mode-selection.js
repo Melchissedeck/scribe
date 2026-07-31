02331a98219c9c2090404b56bfe1f2a36b254f6e
Melchissedeck <afoudahmelchissedeck@gmail.com>
2026-08-01 01:12:15 +0200

    feat: add mode selection screen protected by consent

---
+++ frontend/js/mode-selection.js
@@ -0,0 +1,16 @@
+﻿// Logique de la page de choix du mode de captation
+
+import { requireConsent } from './consent.js';
+
+requireConsent();
+
+const visioOption = document.getElementById('visio-option');
+const dictaOption = document.getElementById('dicta-option');
+
+visioOption.addEventListener('click', () => {
+  window.location.href = 'visio.html';
+});
+
+dictaOption.addEventListener('click', () => {
+  window.location.href = 'dicta.html';
+});
